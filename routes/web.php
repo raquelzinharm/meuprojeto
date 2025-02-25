977949b4ed9aee1373c30034e6e7397965f986d8
raquelzinharm <raquelrmelo01@gmail.com>
2025-02-24 21:26:31 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/produto', [ProdutoController::class, 'index']);
