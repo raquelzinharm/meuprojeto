fa393a0e13746f05c03c20838999c8ab4f354d6d
raquelzinharm <raquelrmelo01@gmail.com>
2025-03-17 21:23:03 -0300

    DatabaseSeeder

---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('produtos')->insert([
+           'name' => 'Sapato',
+           'quantidade' => 10,
+           'valor' => 100.10,
+
+        ]);
+    }
+}
