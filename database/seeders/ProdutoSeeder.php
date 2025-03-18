4da4f538b733506e7fbf413b54a3a5611988e0d8
raquelzinharm <raquelrmelo01@gmail.com>
2025-03-17 21:46:32 -0300

    migrate e seed

--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProdutoSeeder extends Seeder
 {
@@ -13,10 +14,22 @@ class ProdutoSeeder extends Seeder
     public function run(): void
     {
         DB::table('produtos')->insert([
-           'name' => 'Sapato',
+           'nome' => 'Sapato',
            'quantidade' => 10,
            'valor' => 100.10,
 
         ]);
+        DB::table('produtos')->insert([
+            'nome' => 'Tênis',
+            'quantidade' => 20,
+            'valor' => 150.99,
+
+         ]);
+         DB::table('produtos')->insert([
+            'nome' => 'Casaco',
+            'quantidade' => 50,
+            'valor' => 80.99,
+
+         ]);
     }
 }
