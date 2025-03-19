1e68f8e99082656633c0447a2223390942f8a58c
raquelzinharm <raquelrmelo01@gmail.com>
2025-03-19 19:35:17 -0300

    .env

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
     public function index () {
-        dd('Primeiro Teste');
+
+       $produtos = Produto::orderBy('nome', 'ASC')->get();
+       return view ('produto.produto_index', ['produtos' => $produtos]);
+
+       // dd($produtos);
+
+       //$produto = Produto::find(3);
+
+       //dd($produto);
+        //dd('Primeiro Teste');
     }
 }
---
+++ resources/views/produto/produto_index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Produto </title>
+
+</head>
+
+<body>
+
+<h1>Listagem de produtos </h1>
+<table>
+
+<tr>
+<th> id</th>
+<th> nome</th>
+<th> quantidade</th>
+<th> valor</th>
+
+</tr>
+
+</table>
+
+</body>
+</html>
